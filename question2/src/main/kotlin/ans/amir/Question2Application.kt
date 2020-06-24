60b08887965461f6687b7f3ef0a6f75e6bb992c5
amirsaeedr <amirsaeedranjbar@gmail.com>
2020-06-24 17:50:21 +0430

    Add Spring dep to q2

---
+++ question2/src/main/kotlin/ans/amir/Question2Application.kt
@@ -0,0 +1,11 @@
+package ans.amir
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Question2Application
+
+fun main(args: Array<String>) {
+    runApplication<Question2Application>(*args)
+}
