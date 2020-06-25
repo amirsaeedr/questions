c8144a500490f6f87b9274defb3bf9473bff4013
amirsaeedr <amirsaeedranjbar@gmail.com>
2020-06-25 16:08:46 +0430

    Solve q3

--- question3/loadbalancer/loadbalancer/settings.py
+++ question3/loadbalancer/loadbalancer/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'request_router.apps.RequestRouterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
